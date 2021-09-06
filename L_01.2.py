da2a5d2b1cfeb2ce759b215bd697ec38219993b3
Ilya <mr-yo@mail.ru>
2021-09-06 23:25:31 +0300

    start

--- L_01.2.py
+++
@@ -1,9 +0,0 @@
-#2. Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс. Используйте форматирование строк.
-def timer(start):
-    hours = start//3600
-    remain1= start-3600*hours
-    minutes = remain1//60
-    remain2= remain1-60*minutes
-    print(f'{hours}:{minutes}:{remain2}')
-
-timer(int(input('Введите время в секундах: ')))
